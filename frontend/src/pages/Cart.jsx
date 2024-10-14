589442ecbf7bc6ed5f300c05fcfb0939a479cf3a
Shashank <tripathishashank12903@gmail.com>
2024-10-15 04:08:53 +0530

    no login

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -66,7 +66,6 @@ const Cart = () => {
       <div className='flex justify-end my-20'>
         <div className='w-full sm:w-[450px]'>
           <CartTotal />
-          <p>Please Ensure You Are Logged In Before Proceeding To Checkout </p>
           <div className=' w-full text-end'>
             <button onClick={() => navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO CHECKOUT</button>
           </div>
