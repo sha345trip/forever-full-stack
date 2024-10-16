a1f1437e7a081da0faddf325583370277322df05
Shashank <tripathishashank12903@gmail.com>
2024-10-16 22:01:40 +0530

    fb pixel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
@@ -19,10 +19,47 @@ import Policy from './pages/Policy'
 import Terms from './pages/Terms'
 import Refund from './pages/Refund'
 import ShippingPolicy from './pages/Shipping'
+import {Helmet} from 'react-helmet';
 
 const App = () => {
+  useEffect(() => {
+    // Initialize the Meta Pixel
+    (function(f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function() {
+        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = '2.0';
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+  
+    fbq('init', '840990351569535'); // Replace 'your-pixel-id-goes-here' with your actual Pixel ID
+    fbq('track', 'PageView');
+    }, []);
+
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Helmet>
+        <noscript>
+            {
+          `<img
+            height="1"
+            width="1"
+            style="display:none"
+            src="https://www.facebook.com/tr?id=840990351569535&ev=PageView&noscript=1"
+            alt="Meta Pixel"
+          />`
+        }
+        </noscript>
+      </Helmet>
       <ToastContainer />
       <Navbar />
       <SearchBar />
