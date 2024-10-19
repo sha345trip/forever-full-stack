a0ccae397c73a2f51d00ca9e962c071ecd5c44fe
Shashank <tripathishashank12903@gmail.com>
2024-10-20 04:14:36 +0530

    minor design

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -18,7 +18,7 @@ const LatestCollection = () => {
     },[products])
 
   return (
-    <div className='my-10'>
+    <div className='mt-2 sm:mt-5 md:mt-7 lg:mt-10'>
       <div className='text-center py-8 text-3xl'>
           <Title text1={'LATEST'} text2={'COLLECTIONS'} />
           <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
